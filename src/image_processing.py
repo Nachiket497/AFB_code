a19df6e98d89d6656048c23d0ac0eb4f757de9a5
Nachiket497 <1000nachiket@gmail.com>
2022-10-23 21:40:28 +0530

    modified the image processing file

--- src/image_processing.py
+++ src/image_processing.py
@@ -10,8 +10,6 @@ def green_threshold(img):
     mask = cv2.inRange(hsv, lower_green, upper_green)
     return mask
 
-# avg filter to smooth out the image
-
 
 def avg_filter(img):
     kernel = (1/10)*np.array([
@@ -24,36 +22,21 @@ def avg_filter(img):
     dst = cv2.filter2D(img, -1, kernel)
     return dst
 
-# single value thresholding
 
-
-def single_value_threshold(img):
-    ret, thresh1 = cv2.threshold(img, 70, 255, cv2.THRESH_BINARY)
+def single_value_threshold(img, cutoff=65):
+    ret, thresh1 = cv2.threshold(img, cutoff, 255, cv2.THRESH_BINARY)
     return thresh1
-
-# take min in the window from the image
 
 
 def min_filter(img):
-    kernel = np.ones(shape=(5, 5), dtype=np.uint8)
+    kernel = np.ones(shape=(3, 3), dtype=np.uint8)
     dst = cv2.erode(img, kernel, iterations=1)
     return dst
-
-# vertical interpolation
-
-
-def vertical_interpolation(img):
-    dst = cv2.resize(img, None, fx=1, fy=1, interpolation=cv2.INTER_LINEAR)
-    return dst
-
-# interpolation nearest neighbour
 
 
 def nearest_neighbour(img):
     dst = cv2.resize(img, None, fx=1, fy=1, interpolation=cv2.INTER_NEAREST)
     return dst
-
-# morphological operations dilation if m nor wrong
 
 
 def dilation(img):
@@ -62,62 +45,81 @@ def dilation(img):
     return dst
 
 
-def erosion(img):
+def opening(img):
     kernel = np.ones(shape=(5, 5), dtype=np.uint8)
-    dst = cv2.erode(img, kernel, iterations=1)
+    dst = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
     return dst
 
-# draw a red pixel at the avg position of white pixels in each row
 
-
-def draw_line(img):
+def avg_white(img):
+    x, y = [], []
+    for i in range(len(img)):
+        for j in range(len(img[0])):
+            if img[i][j] == 255:
+                x.append(i)
+                y.append(j)
     img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-    for i in range(img.shape[0]):
-        white_pixels = np.where(img[i, :, 0] == 255)
-        if len(white_pixels[0]) > 0:
-            avg = int(np.mean(white_pixels))
-            img[i, avg, :] = [0, 0, 255]
+    x, y = sum(x)//len(x), sum(y)//len(y)
+    # img[x][y] = [0, 0, 255]
+    img = draw_stline(img, x, y, [255, 0, 0])
+    return img, x, y
+
+
+def divide_avg(img):
+    x, y = img.shape
+    posx, posy = [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]
+    img_new = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+    img_new[:x//3, :(y//2)], posx[0], posy[0] = avg_white(img[:x//3, :(y//2)])
+    img_new[:x//3, y//2:], posx[1], posy[1] = avg_white(img[:x//3, y//2:])
+    img_new[x//3:2*x//3, :y //
+            2], posx[2], posy[2] = avg_white(img[x//3:2*x//3, :y//2])
+    img_new[x//3:2*x//3, y //
+            2:], posx[3], posy[3] = avg_white(img[x//3:2*x//3, y//2:])
+    img_new[2*x//3:, :y//2], posx[4], posy[4] = avg_white(img[2*x//3:, :y//2])
+    img_new[2*x//3:, y//2:], posx[5], posy[5] = avg_white(img[2*x//3:, y//2:])
+
+    posx[2] += x//3
+    posx[3] += x//3
+    posx[4] += 2*x//3
+    posx[5] += 2*x//3
+    posy[1] += y//2
+    posy[3] += y//2
+    posy[5] += y//2
+
+    return img_new, [posx, posy]
+
+
+def draw_stline(img, posx, posy, color1=[0, 0, 255]):
+    for i in range(len(img)):
+        img[i, posy] = color1
     return img
 
 
-if __name__ == '__main__':
-    # read the image
+if __name__ == "__main__":
+
     img = cv2.imread('test.png')
     # threshold the image
     mask = green_threshold(img)
-    for _ in range(3):
-        # mask = nearest_neighbour(mask)
+    for _ in range(5):
+        mask = nearest_neighbour(mask)
+        mask = min_filter(mask)
         mask = avg_filter(mask)
         mask = single_value_threshold(mask)
-        mask = dilation(mask)
-        mask = erosion(mask)
+        mask = opening(mask)
+    mask = dilation(mask)
+    mask = dilation(mask)
+    mask = opening(mask)
+    mask = avg_filter(mask)
+    mask = single_value_threshold(mask, 100)
 
-    mask = erosion(mask)
-    mask = erosion(mask)
-    mask = erosion(mask)
-    mask = dilation(mask)
-    mask = erosion(mask)
-    mask = dilation(mask)
-    mask = erosion(mask)
-    mask = dilation(mask)
-    mask = min_filter(mask)
-    mask = dilation(mask)
+    # mask = avg_white(mask)
+    mask, pos = divide_avg(mask)
+    mask = draw_stline(mask, sum(pos[0][2:4])//2, sum(pos[1][2:4])//2)
+    mask = draw_stline(mask, sum(pos[0][4:6])//2, len(mask[0])//2, [0, 255, 0])
 
-    for _ in range(3):
-        # mask = nearest_neighbour(mask)
-        mask = avg_filter(mask)
-        mask = single_value_threshold(mask)
-        mask = dilation(mask)
-        mask = erosion(mask)
-
-    # mask = mirror_image(mask)
-    mask = single_value_threshold(mask)
-    mask = dilation(mask)
-    mask = draw_line(mask)
-    # mask = draw_st_line(mask)
     try:  # show the images
         cv2.imshow('image', img)
         cv2.imshow('mask', mask)
-        cv2.waitKey(5000)
+        cv2.waitKey(10000)
     except KeyboardInterrupt:
         cv2.destroyAllWindows()
